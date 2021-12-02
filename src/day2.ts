9b8398410347a9a67f5819844d3f4c311cfed9e1
NuclearRedeye <github@mneah.com>
2021-12-02 08:45:40 +0000

    chore: minor refactor of day2 code

--- src/day2.ts
+++ src/day2.ts
@@ -7,50 +7,7 @@ type Vector = {
 
 const wordSpaceDigit = new RegExp(/^(\w+) (\d+)$/);
 
-function getMoveAsVector(data: string) : Vector {
-  let retVal = {
-    x: 0,
-    y: 0,
-    z: 0
-  };
-
-  const match = data.match(wordSpaceDigit);
-  if (match) {
-    switch (match[1]) {
-      case 'forward':
-        retVal.x += parseInt(match[2]);
-        break;
-      case 'up':
-        retVal.y -= parseInt(match[2]);
-        break;
-      case 'down':
-        retVal.y += parseInt(match[2]);
-        break;
-      default:
-        console.error(`getMoveAsVector(): invalid input '${data}'`);
-    }
-  }
-
-  return retVal;
-}
-
-export function part1(data: string[]): number {
-  const retVal: Vector = {
-    x: 0,
-    y: 0,
-    z: 0
-  };
-
-  for (let i = 0; i < data.length; i++) {
-    const move = getMoveAsVector(data[i]);
-    retVal.x += move.x;
-    retVal.y += move.y;
-  }
-
-  return retVal.x * retVal.y;
-}
-
-function getMoveAsVector2(data: string, vector: Vector) {
+function getMoveAsVector(data: string, vector: Vector) {
   const match = data.match(wordSpaceDigit);
   if (match) {
     switch (match[1]) {
@@ -70,6 +27,20 @@ function getMoveAsVector2(data: string, vector: Vector) {
   }
 }
 
+export function part1(data: string[]): number {
+  const retVal: Vector = {
+    x: 0,
+    y: 0,
+    z: 0
+  };
+
+  for (let i = 0; i < data.length; i++) {
+    getMoveAsVector(data[i], retVal);
+  }
+
+  return retVal.x * retVal.z;
+}
+
 export function part2(data: string[]): number {
   const retVal: Vector = {
     x: 0,
@@ -78,7 +49,7 @@ export function part2(data: string[]): number {
   };
 
   for (let i = 0; i < data.length; i++) {
-    getMoveAsVector2(data[i], retVal);
+    getMoveAsVector(data[i], retVal);
   }
 
   return retVal.x * retVal.y;
